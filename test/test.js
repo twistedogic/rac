5a4afde1be33373ab4c6c9801315a55088c9c282
root <root@66534667732b.(none)>
2015-08-17 11:00:02 +0000

    add scheduler

--- test/test.js
+++ test/test.js
@@ -1,16 +1,99 @@
 var _ = require('lodash');
-var bard = require('../lib/bard.js');
+var assert = require('assert');
+var fs = require('fs');
+var sample = fs.readFileSync('../sample/matches1.json');
+sample = JSON.parse(sample);
+sample = _.sample(sample.matches);
+var match = require('../lib/match.js');
+var staticData = require('../lib/staticData.js');
+var data = require('../lib/data.js');
+var createTarget = require('../lib/util/createTarget.js');
 describe("Scrape Data",function(){
-    it("match data",function(done){
-        bard({
-            id:1907069332,
-            type:'match',
-            region:'na',
-            timeout:0
+    it("normal match data",function(done){
+        match({
+            id:1852548676,
+            region:'na'
         },function(err,res){
             if(_.isObject(res)){
                 done();
             }
         })
+    });
+    it("ranked match data",function(done){
+        match({
+            id:1852538938,
+            region:'na'
+        },function(err,res){
+            if(_.isObject(res)){
+                done();
+            }
+        })
+    });
+    it("champion data",function(done){
+        staticData({
+            id:34,
+            type:'champion',
+            version:"5.15.1"
+        },function(err,res){
+            if(_.isObject(res)){
+                done();
+            }
+        })
+    });
+    it("item data",function(done){
+        staticData({
+            id:3303,
+            type:'item',
+            version:"5.15.1"
+        },function(err,res){
+            if(_.isObject(res)){
+                done();
+            }
+        })
+    });
+    it("mastery data",function(done){
+        staticData({
+            id:3303,
+            type:'mastery',
+            version:"5.15.1"
+        },function(err,res){
+            if(_.isObject(res)){
+                done();
+            }
+        })
+    });
+    it("rune data",function(done){
+        staticData({
+            id:3303,
+            type:'rune',
+            version:"5.15.1"
+        },function(err,res){
+            if(_.isObject(res)){
+                done();
+            }
+        })
+    });
+    it("summoner data",function(done){
+        staticData({
+            id:3303,
+            type:'summoner',
+            version:"5.15.1"
+        },function(err,res){
+            if(_.isObject(res)){
+                done();
+            }
+        })
+    });
+})
+
+describe("Process Data",function(){
+    it("punch flat the data",function(){
+        _.isObject(data(sample));
+    });
+})
+
+describe("Scheduler",function(){
+    it("create targets",function(){
+        _.isArray(createTarget);
     });
 })
